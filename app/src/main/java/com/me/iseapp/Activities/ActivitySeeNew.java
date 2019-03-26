62596d6639e943ff09f5cf8dc26ac654c09357e2
paxi07 <paxi070@gmail.com>
2019-03-26 14:39:56 +0000

    26/03/2019

---
+++ app/src/main/java/com/me/iseapp/Activities/ActivitySeeNew.java
@@ -0,0 +1,6 @@
+package com.me.iseapp.Activities;
+
+public class ActivitySeeNew
+{
+
+}
--- app/src/main/java/com/me/iseapp/FragmentsMenu/FragmentMenuAttendance.java
+++ app/src/main/java/com/me/iseapp/FragmentsMenu/FragmentMenuAttendance.java
@@ -1,5 +1,6 @@
 package com.me.iseapp.FragmentsMenu;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,19 +8,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.me.iseapp.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FragmentMenuAttendance extends Fragment
 {
+    WebView mWebView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_menu_attendance, container, false);
 
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+        /*Intent browserIntent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("https://iseireland.ie/cms/attendance"));
-        startActivity(browserIntent);
+        startActivity(browserIntent);*/
+
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        final Timer t = new Timer();
+        t.schedule(new TimerTask()
+        {
+            public void run()
+            {
+                progressDialog.dismiss();
+                t.cancel();
+            }
+        }, 2000);
+
+
+        mWebView = (WebView) view.findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://iseireland.ie/cms/attendance");
 
         return view;
     }
